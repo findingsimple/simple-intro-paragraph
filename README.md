42f2a6c3151eb64a2105aad3313494f54fa71f4f
Jason Conroy <jason@findingsimple.com>
2013-09-12 17:02:17 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Simple Intro Paragraph #
+
+Contributors: jconroy    
+Tags: editor, custom, style, paragraph, intro, introduction, tinymce  
+Requires at least: 3.6  
+Tested up to: 3.6.1  
+Stable tag: 1.0
+
+Adds an "Intro Paragraph" style option to the styles dropdown in the WordPress editor.
+
+## Description ##
+
+Adds an "Intro Paragraph" style option to the styles dropdown in the WordPress editor.
+
+## Installation ##
+
+1. Copy the 'simple-intro-paragraph' folder into your plugins folder
+2. Activate the plugin via the Plugins admin page
+
+## Usage ##
+
+This plugin adds 'Intro Paragaph' as an option in the style dropdown. 
+
+The plugin DOES NOT add any styles to the dashboard. Theme developers can use the ```add_editor_style();``` function to link a custom stylesheet file specific to their needs to the TinyMCE visual editor.
+
+The class applied to the paragraph is ```intro-paragraph```.
+
+## Changelog ##
+
+### 1.0 ###
+* Initial release
+
---
+++ simple-intro-paragraph.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Simple Intro Paragraph
+Plugin URI: http://plugins.findingsimple.com
+Description: Simple plugin for addin an "intro" paragraph style to the wordpress editor for styling 
+Version: 1.0
+Author: Finding Simple
+Author URI: http://findingsimple.com
+License: GPL2
+*/
+/*
+Copyright 2013  Finding Simple  (email : plugins@findingsimple.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! class_exists( 'Simple_Intro_Paragraph' ) ) :
+
+/**
+ * Plugin Main Class.
+ *
+ */
+class Simple_Intro_Paragraph {
+    
+    /**
+     * Initialise
+     */
+    function Simple_Intro_Paragraph() {
+        
+        add_action( 'init', array( &$this , 'simple_intro_paragraph_filter' ) );
+
+    }
+
+    /**
+     * Apply appropriate hooks and filters to add the intro style
+     */
+    function simple_intro_paragraph_filter() {
+
+        add_filter( 'mce_buttons_2', array( &$this , 'add_styles_dropdown' ) ); 
+
+        add_filter( 'tiny_mce_before_init', array( &$this , 'add_styles_to_dropdown' ) );
+        
+    }  
+
+    /**
+     * Force add the "Styles" drop-down
+     */ 
+    function add_styles_dropdown( $buttons ) {
+
+        if ( !in_array( 'styleselect', $buttons ) )
+            array_unshift( $buttons, 'styleselect' );
+
+        return $buttons;
+
+    }
+
+    /**
+     * Add styles/classes to the "Styles" drop-down
+     */ 
+    function add_styles_to_dropdown( $settings ) {
+
+        $style_formats = array(
+            array(
+                'title' => 'Intro Paragraph',
+                'selector' => 'p',
+                'classes' => 'intro-paragraph',
+            )
+        );
+
+        $settings['style_formats'] = json_encode( $style_formats );
+
+        return $settings;
+
+    }
+
+}
+
+$Simple_Intro_Paragraph = new Simple_Intro_Paragraph();
+
+endif;
